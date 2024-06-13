5aaa274eac56b23eb6c4107a10d135a860453f36
liuhaihua <liuahaihua@idadt.com>
2024-06-13 15:01:59 +0800

    Spring Cloud hystrix

--- hystrix/src/main/java/com/et/hystrix/controller/HelloWorldController.java
+++ hystrix/src/main/java/com/et/hystrix/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.et.hystrix.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +12,19 @@ import java.util.Map;
 @RestController
 public class HelloWorldController {
     @RequestMapping("/hello")
-    public Map<String, Object> showHelloWorld(){
+    @HystrixCommand(fallbackMethod = "fallback_hello", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+    })
+    public Map<String, Object> showHelloWorld() throws InterruptedException {
+        Thread.sleep(3000);
         Map<String, Object> map = new HashMap<>();
         map.put("msg", "HelloWorld");
+        return map;
+    }
+
+    private Map<String, Object> fallback_hello() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "Request fails. It takes long time to response");
         return map;
     }
 }
