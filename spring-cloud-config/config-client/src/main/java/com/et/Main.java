68d21ac856f3fcc90d214a79a105cf30ce755a68
liuhaihua <liuahaihua@idadt.com>
2024-11-01 14:53:41 +0800

    Spring Cloud config

--- spring-cloud-config/config-client/src/main/java/com/et/Main.java
+++
@@ -1,7 +0,0 @@
-package com.et;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ spring-cloud-config/config-client/src/main/java/com/et/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.et.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuhaihua
+ * @version 1.0
+ * @ClassName DemoController
+ * @Description todo
+ * @date 2024/11/01/ 13:47
+ */
+@RestController
+public class DemoController {
+    @Value("${example.property}")
+    private String exampleProperty;
+
+    @GetMapping("/property")
+    public String getProperty() {
+        return exampleProperty;
+    }
+}
