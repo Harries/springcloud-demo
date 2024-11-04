5b0809cd8f0c430df5848125392af097d4a04ebc
liuhaihua <liuahaihua@idadt.com>
2024-11-04 11:24:09 +0800

    Spring Cloud bus

---
+++ spring-cloud-bus/bus-app1/src/main/java/com/et/Bus1Application.java
@@ -0,0 +1,17 @@
+package com.et;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bus1Application {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bus1Application.class, args);
+    }
+
+
+}
---
+++ spring-cloud-bus/bus-app2/src/main/java/com/et/Bus2Application.java
@@ -0,0 +1,17 @@
+package com.et;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bus2Application {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bus2Application.class, args);
+    }
+
+
+}
