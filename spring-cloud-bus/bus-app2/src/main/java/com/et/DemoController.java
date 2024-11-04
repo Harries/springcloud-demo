b0c261188d86e479a6957c268a5b0103dd3616f7
liuhaihua <jxausea@163.com>
2024-11-04 21:04:45 +0800

    spring cloud bus

--- spring-cloud-bus/bus-app2/src/main/java/com/et/DemoController.java
+++ spring-cloud-bus/bus-app2/src/main/java/com/et/DemoController.java
@@ -1,6 +1,7 @@
 package com.et;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2024/11/01/ 13:47
  */
 @RestController
+@RefreshScope
 public class DemoController {
     @Value("${example.property}")
     private String exampleProperty;
